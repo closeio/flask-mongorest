b0c30e485f5c4714be39448df7ffa28991caaf0c
Thomas Steinacher <tom@eggdrop.ch>
2012-08-30 21:29:06 -0700

    Fix semantics of response_success/response_error

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,15 +3,17 @@ import unittest
 import example.app as example
 
 
-def response_success(response, code=200):
-    if 200 <= code < 300:
+def response_success(response, code=None):
+    if code is None:
         assert 200 <= response.status_code < 300, 'Received %d response: %s' % (response.status_code, response.data)
-    assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
+    else:
+        assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
 
-def response_error(response, code=400):
-    if 400 <= code < 500:
+def response_error(response, code=None):
+    if code is None:
         assert 400 <= response.status_code < 500, 'Received %d response: %s' % (response.status_code, response.data)
-    assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
+    else:
+        assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
 
 def compare_req_resp(req_obj, resp_obj):
     for k,v in req_obj.iteritems():
