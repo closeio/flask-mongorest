d43876fd8c2aba670416828f9c4d707e8c7819ae
Thomas Steinacher <tom@eggdrop.ch>
2013-05-25 20:16:45 +0200

    Performance: Don't load raw_data every time we initialize a resource.

--- flask_mongorest/resources.py
+++ flask_mongorest/resources.py
@@ -54,15 +54,23 @@ class Resource(object):
         self._filters = self.get_filters()
         self._child_document_resources = self.get_child_document_resources()
         self.data = None
-        self.raw_data = {}
         self._dirty_fields = None
         if request.method in ('PUT', 'POST'):
             if request.mimetype and 'json' not in request.mimetype:
                 raise ValidationError({'error': "Please send valid JSON with a 'Content-Type: application/json' header."})
-            try:
-                self.raw_data = json.loads(request.data)
-            except ValueError, e:
-                raise ValidationError({'error': 'The request contains invalid JSON.'})
+
+    @property
+    def raw_data(self):
+        if not hasattr(self, '_raw_data'):
+            if request.method in ('PUT', 'POST'):
+                try:
+                    self._raw_data = json.loads(request.data)
+                except ValueError, e:
+                    raise ValidationError({'error': 'The request contains invalid JSON.'})
+            else:
+                self._raw_data = {}
+
+        return self._raw_data
 
     @classmethod
     def uri(self, path):
