52388d7e18ff3b0dfc8b4c26888b759a1e59d80d
Andrew Rodionoff <andviro@gmail.com>
2014-12-04 09:47:19 +0300

    added some documentation references

--- flask_mongorest/__init__.py
+++ flask_mongorest/__init__.py
@@ -1,30 +1,56 @@
 from flask import Blueprint
 from flask_mongorest.methods import Create, BulkUpdate, List
 
-class DelayedApp(object):
+
+class _DelayedApp(object):
+
+    '''
+    Stores URL rules for later merging with application URL map.
+
+    '''
+
     def __init__(self):
         self.url_rules = []
 
     def add_url_rule(self, *args, **kwargs):
         self.url_rules.append((args, kwargs))
 
+
 class MongoRest(object):
 
     def __init__(self, app=None, **kwargs):
+        '''
+        Takes optional Flask application instance. If supplied, `init_app` will be
+        called to update application url map.
+
+        '''
+
         self.url_prefix = kwargs.pop('url_prefix', '')
         self.template_folder = kwargs.pop('template_folder', 'templates')
         if app is not None:
             self.init_app(app, **kwargs)
         else:
-            self.app = DelayedApp()
+            self.app = _DelayedApp()
 
     def init_app(self, app):
+        '''
+        Provides delayed application instance initialization to support
+        Flask application factory pattern. For further details on application
+        factories see:
+
+        http://flask.pocoo.org/docs/dev/patterns/appfactories/
+
+        and
+
+        http://mattupstate.com/python/2013/06/26/how-i-structure-my-flask-applications.html
+
+        '''
         app.register_blueprint(
             Blueprint(self.url_prefix,
                       __name__,
                       template_folder=self.template_folder))
         if hasattr(self, 'app'):
-            if isinstance(self.app, DelayedApp):
+            if isinstance(self.app, _DelayedApp):
                 for args, kwargs in self.app.url_rules:
                     app.add_url_rule(*args, **kwargs)
         self.app = app
@@ -32,7 +58,8 @@ class MongoRest(object):
     def register(self, **kwargs):
         def decorator(klass):
 
-            # Construct a url based on a 'name' kwarg with a fallback to a Mongo document's name
+            # Construct a url based on a 'name' kwarg with a fallback to a
+            # Mongo document's name
             document_name = klass.resource.document.__name__.lower()
             name = kwargs.pop('name', document_name)
             url = kwargs.pop('url', '/%s/' % document_name)
@@ -45,10 +72,28 @@ class MongoRest(object):
             pk_type = kwargs.pop('pk_type', 'string')
             view_func = klass.as_view(name)
             if List in klass.methods:
-                self.app.add_url_rule(url, defaults={'pk': None}, view_func=view_func, methods=[List.method], **kwargs)
+                self.app.add_url_rule(
+                    url,
+                    defaults={
+                        'pk': None},
+                    view_func=view_func,
+                    methods=[
+                        List.method],
+                    **kwargs)
             if Create in klass.methods or BulkUpdate in klass.methods:
-                self.app.add_url_rule(url, view_func=view_func, methods=[x.method for x in klass.methods if x in (Create, BulkUpdate)], **kwargs)
-            self.app.add_url_rule('%s<%s:%s>/' % (url, pk_type, 'pk'), view_func=view_func, methods=[x.method for x in klass.methods if x not in (List, BulkUpdate)], **kwargs)
+                self.app.add_url_rule(
+                    url,
+                    view_func=view_func,
+                    methods=[
+                        x.method for x in klass.methods if x in (
+                            Create,
+                            BulkUpdate)],
+                    **kwargs)
+            self.app.add_url_rule(
+                '%s<%s:%s>/' %
+                (url, pk_type, 'pk'), view_func=view_func, methods=[
+                    x.method for x in klass.methods if x not in (
+                        List, BulkUpdate)], **kwargs)
             return klass
 
         return decorator
