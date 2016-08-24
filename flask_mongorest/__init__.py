a8804b51f3a37ddccecbed1d97f892cc9b635b9e
Frank Valcarcel <frank@cuttlesoft.com>
2016-08-24 18:17:01 -0400

    settles flake8 visual indent warnings

--- flask_mongorest/__init__.py
+++ flask_mongorest/__init__.py
@@ -68,15 +68,15 @@ class MongoRest(object):
             pk_type = kwargs.pop('pk_type', 'string')
             view_func = klass.as_view(name)
             if List in klass.methods:
-                self.app.add_url_rule(url, defaults={'pk': None}, view_func=view_func,
-                      methods=[List.method], **kwargs)
+                self.app.add_url_rule(
+                    url, defaults={'pk': None}, view_func=view_func, methods=[List.method], **kwargs)
             if Create in klass.methods or BulkUpdate in klass.methods:
                 methods = [x.method for x in klass.methods if x in (Create, BulkUpdate)]
                 self.app.add_url_rule(url, view_func=view_func, methods=methods, **kwargs)
 
             methods = [x.method for x in klass.methods if x not in (List, BulkUpdate)]
-            self.app.add_url_rule('%s<%s:%s>/' % (url, pk_type, 'pk'),
-                view_func=view_func, methods=methods, **kwargs)
+            self.app.add_url_rule(
+                '%s<%s:%s>/' % (url, pk_type, 'pk'), view_func=view_func, methods=methods, **kwargs)
 
             return klass
 
