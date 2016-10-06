26fc8a60523c7d41899b70b68ac8f62c5aac5f7c
Thomas Steinacher <tom@eggdrop.ch>
2016-10-06 12:46:41 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 
 setup(
     name='Flask-MongoRest',
-    version='0.1.1',
+    version='0.2',
     url='http://github.com/closeio/flask-mongorest',
     license='BSD',
     author='Close.io',
