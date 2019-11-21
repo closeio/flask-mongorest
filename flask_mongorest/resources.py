53ce90725b5d722dcea01e1a0138e3734caf548f
Patrick Huck <phuck@lbl.gov>
2019-11-21 15:37:23 -0800

    enable nested _fields

--- flask_mongorest/resources.py
+++ flask_mongorest/resources.py
@@ -4,6 +4,7 @@ import mongoengine
 from bson.dbref import DBRef
 from bson.objectid import ObjectId
 from flask import has_request_context, request, url_for
+from dict_deep import deep_get
 try:
     from urllib.parse import urlparse
 except ImportError: # Python 2
@@ -263,7 +264,7 @@ class Resource(object):
         else:
             for field in only_fields:
                 actual_field = self._reverse_rename_fields.get(field, field)
-                if actual_field in all_fields_set:
+                if actual_field in all_fields_set or any(actual_field.startswith(f) for f in all_fields_set):
                     requested_fields.append(actual_field)
 
         return requested_fields
@@ -366,10 +367,10 @@ class Resource(object):
         if has_field_instance:
             field_value = obj
         elif isinstance(obj, dict):
-            return obj[field_name]
+            return deep_get(obj, field_name)
         else:
             try:
-                field_value = getattr(obj, field_name)
+                field_value = deep_get(obj, field_name)
             except AttributeError:
                 raise UnknownFieldError
 
