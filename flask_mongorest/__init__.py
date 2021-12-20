b9bbdae6a0ad6f73ea26a13d6eeb235a39d5f598
Alec Rosenbaum <alec.rosenbaum@close.io>
2021-12-20 16:03:14 -0500

    split register_class logic out of `MongoRest` class

--- flask_mongorest/__init__.py
+++ flask_mongorest/__init__.py
@@ -3,6 +3,45 @@ from flask import Blueprint
 from flask_mongorest.methods import BulkUpdate, Create, List
 
 
+def register_class(app, klass, *, url_prefix, **kwargs):
+    # Construct a url based on a 'name' kwarg with a fallback to the
+    # view's class name. Note that the name must be unique.
+    name = kwargs.pop("name", klass.__name__)
+    url = kwargs.pop("url", None)
+    if not url:
+        document_name = klass.resource.document.__name__.lower()
+        url = f"/{document_name}/"
+
+    # Insert the url prefix, if it exists
+    if url_prefix:
+        url = f"{url_prefix}{url}"
+
+    # Add url rules
+    pk_type = kwargs.pop("pk_type", "string")
+    view_func = klass.as_view(name)
+    if List in klass.methods:
+        app.add_url_rule(
+            url,
+            defaults={"pk": None},
+            view_func=view_func,
+            methods=[List.method],
+            **kwargs,
+        )
+    if Create in klass.methods or BulkUpdate in klass.methods:
+        app.add_url_rule(
+            url,
+            view_func=view_func,
+            methods=[x.method for x in klass.methods if x in (Create, BulkUpdate)],
+            **kwargs,
+        )
+    app.add_url_rule(
+        f"{url}<{pk_type}:pk>/",
+        view_func=view_func,
+        methods=[x.method for x in klass.methods if x not in (List, BulkUpdate)],
+        **kwargs,
+    )
+
+
 class MongoRest:
     def __init__(self, app, **kwargs):
         self.app = app
@@ -13,46 +52,7 @@ class MongoRest:
 
     def register(self, **kwargs):
         def decorator(klass):
-            # Construct a url based on a 'name' kwarg with a fallback to the
-            # view's class name. Note that the name must be unique.
-            name = kwargs.pop("name", klass.__name__)
-            url = kwargs.pop("url", None)
-            if not url:
-                document_name = klass.resource.document.__name__.lower()
-                url = f"/{document_name}/"
-
-            # Insert the url prefix, if it exists
-            if self.url_prefix:
-                url = f"{self.url_prefix}{url}"
-
-            # Add url rules
-            pk_type = kwargs.pop("pk_type", "string")
-            view_func = klass.as_view(name)
-            if List in klass.methods:
-                self.app.add_url_rule(
-                    url,
-                    defaults={"pk": None},
-                    view_func=view_func,
-                    methods=[List.method],
-                    **kwargs,
-                )
-            if Create in klass.methods or BulkUpdate in klass.methods:
-                self.app.add_url_rule(
-                    url,
-                    view_func=view_func,
-                    methods=[
-                        x.method for x in klass.methods if x in (Create, BulkUpdate)
-                    ],
-                    **kwargs,
-                )
-            self.app.add_url_rule(
-                f"{url}<{pk_type}:pk>/",
-                view_func=view_func,
-                methods=[
-                    x.method for x in klass.methods if x not in (List, BulkUpdate)
-                ],
-                **kwargs,
-            )
+            register_class(self.app, klass, url_prefix=self.url_prefix, **kwargs)
             return klass
 
         return decorator
