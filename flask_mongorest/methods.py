4a7ca196b6856287133b5298f85dc51dfec82b69
Alec Rosenbaum <alec.rosenbaum@close.io>
2021-12-20 15:14:08 -0500

    fix METHODS_TYPE annotation

--- flask_mongorest/methods.py
+++ flask_mongorest/methods.py
@@ -26,4 +26,11 @@ class Delete:
 
 
 # type alias
-METHODS_TYPE = typing.Union[Create, Update, BulkUpdate, Fetch, List, Delete]
+METHODS_TYPE = typing.Union[
+    typing.Type[Create],
+    typing.Type[Update],
+    typing.Type[BulkUpdate],
+    typing.Type[Fetch],
+    typing.Type[List],
+    typing.Type[Delete],
+]
